a544f58468bd18aa397b4d77bef1f3e6a010b181
Andrew Hargrave <andrew@bulletdigitalsolutions.co.uk>
2022-04-12 17:27:48 +0100

    add templating engine config

--- config/config.php
+++ config/config.php
@@ -1,8 +1,17 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Templating Engine
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the templating engine that will be used by the
+    | generated files.
+    | This can either by blade or inertia
+    |
+    */
+    'template_engine' => 'blade',
 
 ];
